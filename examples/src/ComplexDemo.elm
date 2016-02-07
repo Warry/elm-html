896db4261d33601b261738e9cd376306de1f5bd1
Maxime Dantec <hello@warry.fr>
2016-02-07 17:42:21 +0100

    Simplify the demos

--- examples/src/ComplexDemo.elm
+++ examples/src/ComplexDemo.elm
@@ -1,11 +1,15 @@
-module Demo where
+module ComplexDemo where
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Html.Lazy exposing (..)
 import VirtualDom
 
+{-|-}
 ($) = Html.map
+($>) = Html.forward
+
 
 type alias Model
   = (Int, Int)
@@ -14,6 +18,7 @@ type Action
   = NoOp
   | Left ItemAction
   | Right ItemAction
+  | Reset
 
 type ItemAction
   = Inc
@@ -24,43 +29,49 @@ initialModel = (0, 0)
 
 
 view : Model -> Html Action
-view (left, right) =
+view (left, right) addr = addr |> -- Extract the address from the Html msg
   div []
-    [ Left  $ viewItem left
-    , Right $ viewItem right
+    [ Left  $ viewItem left addr
+    , Right $ lazy2 viewItem right addr
     ]
 
 
-viewItem : Int -> Html ItemAction
-viewItem model =
+viewItem : Int -> Signal.Address Action -> Html ItemAction
+viewItem model addr =
   div []
     [ button [ onClick Dec ] [ text "-" ]
     , text (toString model)
     , button [ onClick Inc ] [ text "+" ]
+    -- Use that address :
+    , addr $> button [ onClick Reset ] [ text "reset all" ]
     ]
+
 
 
 update : Action -> Model -> Model
 update action (left, right) =
-    case action of
-      NoOp ->
-        (left, right)
+  case action of
+    NoOp ->
+      (left, right)
 
-      Left a ->
-        (updateItem a left, right)
+    Left a ->
+      (updateItem a left, right)
 
-      Right a ->
-        (left, updateItem a right)
+    Right a ->
+      (left, updateItem a right)
+
+    Reset ->
+      (0,0)
 
 
 updateItem : ItemAction -> Int -> Int
 updateItem action model =
-    case action of
-      Dec ->
-        model - 1
+  case action of
+    Dec ->
+      model - 1
 
-      Inc ->
-        model + 1
+    Inc ->
+      model + 1
 
 
 --
@@ -79,5 +90,5 @@ model =
 main : Signal VirtualDom.Node
 main =
   Signal.map (\m ->
-    view m actions.address
+    lazy view m actions.address
   ) model
